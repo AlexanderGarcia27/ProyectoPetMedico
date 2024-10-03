0e0650c1d249b70102c0c1569edd6fa815a1b9bc
Alexander García <agr270404@gmail.com>
2024-10-02 18:40:11 -0600

    Creacion de la tabla version 3

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -17,7 +17,7 @@ export default function Inicio() {
                         <span className="text-2xl font-semibold whitespace-nowrap dark:text-black">PetMedico</span>
                     </div>
                     <div className="flex items-center space-x-4">
-                        <select className="p-2 bg-white rounded-lg text-sm dark:text-gray-400">
+                        <select className="p-2 bg-white rounded-lg text-sm text-black">
                             <option value="option1">Clientes</option>
                             <option value="option2">Mascotas</option>
                         </select>
@@ -43,7 +43,7 @@ export default function Inicio() {
                             <input
                                 type="text"
                                 id="search-navbar"
-                                className="block w-96 p-2 ps-10 text-sm rounded-lg bg-white dark:placeholder-gr-400 dark:text-white"
+                                className="block w-96 p-2 ps-10 text-sm rounded-lg bg-white dark:placeholder-black text-black"
                                 placeholder="Search..."
                             />
                         </div>
@@ -79,69 +79,73 @@ export default function Inicio() {
                     </div>
                 </div>
             </nav>
-            <div className="relative overflow-x-auto">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Product name
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Color
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Category
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Price
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td className="px-6 py-4">
-                                White
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop PC
-                            </td>
-                            <td className="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td className="px-6 py-4">
-                                Black
-                            </td>
-                            <td className="px-6 py-4">
-                                Accessories
-                            </td>
-                            <td className="px-6 py-4">
-                                $99
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="h-screen w-screen flex justify-center items-center min-h-screen bg-gray-50">
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-[1200px]">
+                    <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 border-collapse">
+                        <thead className="text-xs uppercase  bg-primario text-black">
+                            <tr>
+                                <th scope="col" className="px-6 py-3 border border-black 200">
+                                    Cliente
+                                </th>
+                                <th scope="col" className="px-6 py-3 border border-black 200">
+                                    Mascota
+                                </th>
+                                <th scope="col" className="px-6 py-3 border border-black 200">
+                                    Descripcion
+                                </th>
+                                <th scope="col" className="px-6 py-3 border border-black 200">
+                                    Costo
+                                </th>
+                                <th scope="col" className="px-6 py-3 border border-black 200">
+                                    Precio
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-fondo">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border border-black"></th>
+                                <td className="px-6 py-4 border border-black"></td>
+                                <td className="px-6 py-4 border border-black"></td>
+                                <td className="px-6 py-4 border border-black"></td>
+                                <td className="px-6 py-4 border border-black"></td>
+                            </tr>
+                            <tr className="bg-fondo">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border border-black">
+                                    
+                                </th>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                            </tr>
+                            <tr className="bg-fondo">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border border-black">
+                                    
+                                </th>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                                <td className="px-6 py-4 border border-black">
+                                    
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </>
