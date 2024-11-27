63e7f6641bcc3a7ea33f141683813914418c2e63
Luis20Abel20 <luisabelrangelramirez@gmail.com>
2024-11-27 15:23:53 -0600

    buscador de clientes listo

--- src/components/mascotas.jsx
+++ src/components/mascotas.jsx
@@ -16,6 +16,7 @@ export default function Mascotas() {
     const [mascotas, setMascotas] = useState([]);
     const [expandedCard, setExpandedCard] = useState(null);
     const [selectedMascota, setSelectedMascota] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         const fetchDatosMascotas = async () => {
@@ -70,7 +71,13 @@ export default function Mascotas() {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" className="fill-gray-600 mr-3 rotate-90">
                             <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
                         </svg>
-                        <input type="text" className="w-full outline-none bg-transparent text-gray-600 text-sm" />
+                        <input
+                            type="text"
+                            className="w-full outline-none bg-transparent text-gray-600 text-sm"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            placeholder="Buscar mascota..."
+                        />
                     </div>
                 </div>
             </nav>
@@ -78,7 +85,11 @@ export default function Mascotas() {
             <div className="pt-[120px]">
                 <div className="flex justify-center items-center">
                     <div className="flex flex-wrap justify-center gap-6">
-                        {mascotas.map((mascota, index) => (
+                    {mascotas
+                            .filter(mascota =>
+                                mascota.nombre.toLowerCase().includes(searchTerm.toLowerCase()) // Filtra por nombre
+                            )
+                            .map((mascota, index) => (
                             <div
                                 key={index}
                                 className={`flex flex-col justify-center mx-6 my-6 bg-primario shadow-sm border border-slate-200 rounded-[20px] p-6 w-60 ${expandedCard === index ? 'h-auto' : 'h-[400px]'} transition-all duration-300`}
